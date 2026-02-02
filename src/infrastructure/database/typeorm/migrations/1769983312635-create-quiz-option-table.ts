18900c6649f5ac861d6d7f8501d073679e096ee6
pedro-galdino <pedro.galdino34@gmail.com>
2026-02-01 23:14:28 -0300

    feat: add quiz and quiz item migrations and models

---
+++ src/infrastructure/database/typeorm/migrations/1769983312635-create-quiz-option-table.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateQuizOptionTable1769983312635 implements MigrationInterface {
+  name = 'CreateQuizOptionTable1769983312635';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(/*sql*/ `
+      CREATE TABLE quiz_options (
+        id SERIAL NOT NULL,
+        quiz_item_id INTEGER NOT NULL,
+        text VARCHAR NOT NULL,
+        is_correct BOOLEAN NOT NULL,
+        position INTEGER NOT NULL,
+        CONSTRAINT pk_quiz_options_id PRIMARY KEY (id)
+      );
+    `);
+
+    await queryRunner.query(/*sql*/ `
+      ALTER TABLE quiz_options ADD CONSTRAINT fk_quiz_options_quiz_item_id 
+      FOREIGN KEY (quiz_item_id) REFERENCES quiz_items(id) ON DELETE CASCADE ON UPDATE NO ACTION;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(/*sql*/ `
+      ALTER TABLE quiz_options DROP CONSTRAINT fk_quiz_options_quiz_item_id;
+    `);
+
+    await queryRunner.query(/*sql*/ `
+      DROP TABLE quiz_options;
+    `);
+  }
+}
---
+++ src/infrastructure/database/typeorm/models/quiz-option/quiz-option.model.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { IQuizOption } from '@domain/entities/quiz-option/quiz-option.interface';
+
+import { QuizItemModel } from '../quiz-item/quiz-item.model';
+
+@Entity({ name: 'quiz_options' })
+export class QuizOptionModel implements IQuizOption {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'quiz_item_id', type: 'int' })
+  quizItemId: number;
+
+  @ManyToOne(() => QuizItemModel, (quizItem) => quizItem.options, { onDelete: 'CASCADE', orphanedRowAction: 'delete' })
+  @JoinColumn({ name: 'quiz_item_id', foreignKeyConstraintName: 'fk_quiz_options_quiz_item_id' })
+  quizItem?: QuizItemModel;
+
+  @Column({ name: 'text', type: 'varchar' })
+  text: string;
+
+  @Column({ name: 'is_correct', type: 'boolean' })
+  isCorrect: boolean;
+
+  @Column({ name: 'position', type: 'int' })
+  position: number;
+}
