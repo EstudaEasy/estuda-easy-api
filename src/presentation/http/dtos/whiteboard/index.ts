2359827d256e72678e9fdc1fbd3e15ea86a45eef
Igor Costa <73038518+Igordcs@users.noreply.github.com>
2026-02-15 14:48:20 -0300

    Revert "Feature/create whiteboard crud"

--- src/presentation/http/dtos/whiteboard/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './create-whiteboard.dto';
-export * from './find-whiteboard.dto';
-export * from './find-one-whiteboard.dto';
-export * from './update-whiteboard.dto';
-export * from './delete-whiteboard.dto';
-export * from './whiteboard-response.dto';
