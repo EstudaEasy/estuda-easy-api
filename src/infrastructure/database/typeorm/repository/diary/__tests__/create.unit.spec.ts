d2e05724f2d699691794ac84fffbb0705294c204
pedro-galdino <pedro.galdino34@gmail.com>
2026-02-18 19:06:51 -0300

    feat: add unit tests for diary repository

---
+++ src/infrastructure/database/typeorm/repository/diary/__tests__/create.unit.spec.ts
@@ -0,0 +1,59 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { DiaryModel } from '@database/typeorm/models/diary/diary.model';
+import { TypeOrmUtilsService } from '@database/typeorm/utils/typeorm-utils.service';
+import { DiaryMock } from '@domain/entities/diary/__mocks__/diary.mock';
+
+import { DiaryRepository } from '../diary.repository';
+
+describe('TypeOrm -> Diary Repository -> Create', () => {
+  const diaryMock = new DiaryMock();
+
+  let diaryRepository: DiaryRepository;
+  let diaryRepositoryMock: Repository<DiaryModel>;
+  let createSpy: jest.SpyInstance;
+  let saveSpy: jest.SpyInstance;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        DiaryRepository,
+        TypeOrmUtilsService,
+        {
+          provide: getRepositoryToken(DiaryModel),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn()
+          }
+        }
+      ]
+    }).compile();
+
+    diaryRepository = module.get<DiaryRepository>(DiaryRepository);
+    diaryRepositoryMock = module.get<Repository<DiaryModel>>(getRepositoryToken(DiaryModel));
+
+    createSpy = jest.spyOn(diaryRepositoryMock, 'create');
+    createSpy.mockReturnValue(diaryMock);
+
+    saveSpy = jest.spyOn(diaryRepositoryMock, 'save');
+    saveSpy.mockResolvedValue(diaryMock);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should create and return a diary', async () => {
+    const res = await diaryRepository.create({
+      title: diaryMock.title,
+      content: diaryMock.content,
+      audioUrl: diaryMock.audioUrl
+    });
+
+    expect(res).toStrictEqual(diaryMock);
+    expect(createSpy).toHaveBeenCalledTimes(1);
+    expect(saveSpy).toHaveBeenCalledTimes(1);
+  });
+});
